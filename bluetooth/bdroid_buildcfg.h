aa9772507278cd3c261a5618a11327227fddf052
Sal Savage <salsavage@google.com>
2022-04-15 15:38:17 -0700

    rhodep: Replace BTM_DEF_LOCAL_NAME with a sysprop
    Change-Id: I2c90efac01e61500eb1be23bb0ae6f96c2acdc53

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,7 +20,7 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "moto g82 5G"
+
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    32
