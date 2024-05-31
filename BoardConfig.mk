95c9fefc458ea018144bf2c173f44c05ae32b744
Julian Veit <Claymore1298@gmail.com>
2024-05-31 14:25:06 +0200

    miami: switch to new kernel path
    Change-Id: I37a58c0417f224a93d4f900e8549b876c46179bf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ SOONG_CONFIG_qtidisplay_udfps := true
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=miami
 TARGET_KERNEL_CONFIG := vendor/aicp_miami_defconfig
-TARGET_KERNEL_SOURCE := kernel/motorola/sm6375
+TARGET_KERNEL_SOURCE := kernel/motorola/miami
 
 # Kernel Modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
