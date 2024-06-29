bef1698b04ccfac660f662f7d5c9738be07a3275
Michael Bestas <mkbestas@lineageos.org>
2024-06-29 16:13:50 +0300

    miami: Initial kernel build changes
    Change-Id: Ia8bb8fe65ffdfc249eceef0504eba52272ffa9db

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ SOONG_CONFIG_qtidisplay_udfps := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=miami
-TARGET_KERNEL_CONFIG += vendor/lineage_miami.config
+TARGET_KERNEL_CONFIG += vendor/ext_config/moto-holi-miami.config
 
 # Kernel Modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
