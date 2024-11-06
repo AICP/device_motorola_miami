17f4c17f4ed6563cbfa333434e910c5c3a29bb50
Michael Bestas <mkbestas@lineageos.org>
2024-11-06 14:07:20 +0200

    miami: ruff format --config
    Change-Id: I134c16aaa825feab7aee7057f524818f58603681

--- extract-files.py
+++ extract-files.py
@@ -45,5 +45,7 @@ module = ExtractUtilsModule(
 )
 
 if __name__ == '__main__':
-    utils = ExtractUtils.device_with_common(module, 'sm6375-common', module.vendor)
+    utils = ExtractUtils.device_with_common(
+        module, 'sm6375-common', module.vendor
+    )
     utils.run()
