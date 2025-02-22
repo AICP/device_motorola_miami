a3a6f192bbaf61697bbd76bee20ce362706948b8
Michael Bestas <mkbestas@lineageos.org>
2025-02-22 21:59:59 +0200

    miami: Drop device specific livedisplay manifest
    Change-Id: I7d912d79d1d3542357bef9a7bdb752ecf3860f08

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,6 @@ TARGET_BOOTLOADER_BOARD_NAME := miami
 TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.miami
 SOONG_CONFIG_qtidisplay_udfps := true
 
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=miami
 TARGET_KERNEL_CONFIG += vendor/lineage_miami.config
