dfd443b182219d17d61bdde7217699a35cedc939
Julian Veit <Claymore1298@gmail.com>
2025-04-04 16:10:17 +0200

    miami: switch to kernel config fragments
    Change-Id: I0ae2e5643ea977aa13c543fe130e1d7be69d791b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,8 +39,7 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=miami
-TARGET_KERNEL_CONFIG := vendor/aicp_miami_defconfig
-TARGET_KERNEL_SOURCE := kernel/motorola/miami
+TARGET_KERNEL_CONFIG += vendor/ext_config/moto-holi-miami.config
 
 # Kernel Modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
