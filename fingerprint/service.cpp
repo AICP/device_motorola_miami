ecc3d7af636f4281089afaea7173c8489c4a7382
Julian Veit <Claymore1298@gmail.com>
2025-04-07 08:29:30 +0200

    miami: fingerprint: convert to miami
    Change-Id: Id8a334cdc83c98fda5fbf3db8dfb668f2a71a0a6

--- fingerprint/service.cpp
+++ fingerprint/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.bangkk"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.miami"
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
 #include "BiometricsFingerprint.h"
