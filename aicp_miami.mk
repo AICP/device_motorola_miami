859b7f6d533cecc78c0a12742f44a78c3681bdd4
Julian Veit <claymore1298@gmail.com>
2025-06-06 17:40:46 +0200

    miami: AICPify

--- aicp_miami.mk
+++ aicp_miami.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/miami/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_miami
+PRODUCT_NAME := aicp_miami
 PRODUCT_DEVICE := miami
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
