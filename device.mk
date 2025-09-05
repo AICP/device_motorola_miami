a117dca86c1d4845ca986efcee9b029e24a62e22
Michael Bestas <mkbestas@lineageos.org>
2025-09-05 12:29:24 +0300

    miami: Enable LiveDisplay anti flicker
    Change-Id: Id973d4fc17eb7bbccd6964d89ad718c4f63d2007

--- device.mk
+++ device.mk
@@ -49,6 +49,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.mmi.overlay.rc
 
+# LiveDisplay
+$(call soong_config_set,livedisplay_sysfs,enable_af,true)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
